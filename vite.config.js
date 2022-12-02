88048d299a0013ada40d7c2cbc10f37a3ee72861
Fariaddd <89278138+Fariaddd@users.noreply.github.com>
2022-12-02 01:13:35 +0100

    igitignore

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,9 @@ export default {
     rollupOptions: {
       input: {
         home: resolve(__dirname, "index.html"),
-        signUp: resolve(__dirname, "signup.html"),
+      
         logIn: resolve(__dirname, "signin.html"),
         contact: resolve(__dirname, "contact.html"),
-        createPost: resolve(__dirname, "about.html"),
-        singlePost: resolve(__dirname, "single-post.html"),
-        editPost: resolve(__dirname, "edit-post.html"),
       },
     },
   },
