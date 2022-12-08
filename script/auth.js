74ca81ccb418b44f8b7f26529e9782e85ba3e4ec
Fariaddd <89278138+Fariaddd@users.noreply.github.com>
2022-12-08 10:50:24 +0100

    add some files

--- script/auth.js
+++ script/auth.js
@@ -7,15 +7,26 @@ function headerBar(){
     if(navbar){
         const userName = getUserName();
         let navLinks;
-        navLinks = `
-        <li><a href="/signup.html" class="${pathname ==="/signup.html" ? "" : ""}"</a></li>
-        <li><a href="/signin.html" class="${pathname ==="/signin.html" ? "" : ""}"</a></li>
+        navLinks = navLinks = `
+        <li class="li-navbar"><a href="/index.html" class="${pathname === "/index.html" ? "" : ""}"</a>Home</li>
+        <li class="li-navbar"><a href="/auction.html" class="${pathname === "/about.html" ? "" : ""}"</a>Auction</li>
+        <li class="li-navbar a"><a href="/about.html" class="${pathname === "/contact.html" ? "" : ""}"</a>About</li>
+        <li class="li-navbar"><a href="/contact.html" class="${pathname === "/contact.html" ? "" : ""}"</a>Contact</li> 
+        <li class="li-navbar"><a href="/signup.html" class="${pathname ==="/signup.html" ? "" : ""}"</a>sign-uP</li>
+        <li class="li-navbar"><a href="/signin.html" class="${pathname ==="/signin.html" ? "" : ""}"</a>sing-IN</li>
+        
         `;
         if(userName) {
             navLinks = `
-            <li> <a href="/welcome.html" class="${pathname === "/index.html" ? "" : ""}"></a></li>
-            <li class="p-8"><button id="logOut">LogOut</button></li>
+            
+            <li class="li-navbar" > <a href="/welcome.html" class="${pathname === "/welcome.html" ? "" : ""}"</a>Home</li>
+            <li class="li-navbar" > <a href="/welcome.html" class="${pathname === "/welcome.html" ? "" : ""}"</a>Auction</li>
+
+            <li class="li-navbar" > <a href="/about.html" class="${pathname === "/about.html" ? "" : ""}"</a>About</li>
+            <li class="li-navbar"><button id="logOut">LogOut</button></li>
+            
             `
+            
         }
         navbar.innerHTML = `
         <ul class="flex">
@@ -25,3 +36,12 @@ function headerBar(){
 }
 export {headerBar}
 
+{/*
+        if(userName) {
+            navLinks = `
+            <li class="li-navbar a"><a href="/index.html" class="${pathname === "/index.html" ? "" : ""}"</a>Home</li>
+            <li class="li-navbar a"><a href="/auction.html" class="${pathname === "/about.html" ? "" : ""}"</a>Auction</li>    
+            <li class="li-navbar a"><a href="/contact.html" class="${pathname === "/contact.html" ? "" : ""}"</a>Contact</li>
+            <li class="li-navbar a"><a href="/about.html" class="${pathname === "/contact.html" ? "" : ""}"</a>About</li>
+            <li class="li-navbar a"> <a href="/profile.html" class="${pathname === "/profile.html" ? "text-white" : ""}"</a>Account</li>         
+            <li class=" text-white text-xl "><button id="logOut">Log-out</button></li> */}
